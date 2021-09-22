06144a4d92320c741118e76ffd6c4041f679745a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-09-22 14:52:21 +0100

    dev: ngFor

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    {
+      name: 'Tournevis',
+      price: 2.99,
+      qty: 120,
+    },
+    {
+      name: 'Marteau',
+      price: 7.2,
+      qty: 10,
+    },
+    {
+      name: 'Perceuse',
+      price: 52.1,
+      qty: 6,
+    },
+    {
+      name: 'Pelle',
+      price: 7.8,
+      qty: 23,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
