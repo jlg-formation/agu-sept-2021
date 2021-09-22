2d52c1634b5b0db09957a2c225a081cba99b0803
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-09-22 15:16:07 +0100

    refactor: import with shift+alt+o

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
-
-// the second parameter 'fr' is optional
-registerLocaleData(localeFr, 'fr');
-
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
