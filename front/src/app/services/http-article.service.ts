a2a2bac76d0a5084ff69ec64cb25d1dac1138eb4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-09-23 15:20:13 +0100

    dev: observable operators

--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ArticleService } from './article.service';
 import { Article } from '../interfaces/article';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -14,18 +15,26 @@ export class HttpArticleService extends ArticleService {
   }
 
   refresh() {
-    this.http.get<Article[]>('http://localhost:3000/api/articles').subscribe({
-      next: (articles) => {
-        console.log('articles: ', articles);
-        this.articles = articles;
-        this.save();
-      },
-      complete: () => {
-        console.log('complete');
-      },
-      error: (err) => {
-        console.log('err: ', err);
-      },
-    });
+    this.http
+      .get<Article[]>('http://localhost:3000/api/articles')
+      .pipe
+      // map((articles) => {
+      //   articles.forEach((a) => (a.name = a.name.toUpperCase()));
+      //   return articles;
+      // })
+      ()
+      .subscribe({
+        next: (articles) => {
+          console.log('articles: ', articles);
+          this.articles = articles;
+          this.save();
+        },
+        complete: () => {
+          console.log('complete');
+        },
+        error: (err) => {
+          console.log('err: ', err);
+        },
+      });
   }
 }
