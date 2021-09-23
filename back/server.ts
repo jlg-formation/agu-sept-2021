16de0999699835e3316799e7cc93709e4bf8961e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-09-23 11:37:40 +0100

    dev: added typescript

--- back/server.ts
+++ back/server.ts
@@ -4,7 +4,6 @@ const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
 const dir = "../front/dist/front";
-
 app.use((req, res, next) => {
   console.log("url:", req.url);
   next();
