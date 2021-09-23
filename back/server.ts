94bb2e88d2bf3cdfbd6cceed1d3d88b3366404b0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-09-23 15:08:57 +0100

    dev[back]: implemented get articles

--- back/server.ts
+++ back/server.ts
@@ -1,16 +1,34 @@
+import cors from "cors";
 import express from "express";
 import serveIndex from "serve-index";
-import cors from "cors";
+import { Article } from "../front/src/app/interfaces/article";
 
 const app = express();
 const port = 3000;
 const dir = "../front/dist/front";
+
+const articles: Article[] = [
+  {
+    name: "Tournevis",
+    price: 2.99,
+    qty: 120,
+  },
+  {
+    name: "Marteau",
+    price: 7.2,
+    qty: 10,
+  },
+];
 
 app.use(cors());
 
 app.use((req, res, next) => {
   console.log("url:", req.url);
   next();
+});
+
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
 });
 
 app.use(express.static(dir));
