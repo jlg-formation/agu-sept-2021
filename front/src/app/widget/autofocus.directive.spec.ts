b20806692edcd7d32b6c95ee6f8313964375dca7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-09-24 11:55:51 +0100

    test: all test works

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,40 @@
 import { AutofocusDirective } from './autofocus.directive';
 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `<div>
+    <input name="toto" />
+    <input name="titi" appAutofocus />
+  </div>`,
+})
+export class TestComponent {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutofocusDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should focus', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titi = compiled.querySelector('input[name="titi"');
+    const focus = document.activeElement;
+    expect(titi === focus).toBeTrue();
   });
 });
